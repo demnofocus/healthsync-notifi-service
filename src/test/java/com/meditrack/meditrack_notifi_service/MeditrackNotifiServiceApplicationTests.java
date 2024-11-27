5656918784fad51ce44b624f748033ba7d586a08
Kevin Kularathne <47452568+demnofocus@users.noreply.github.com>
2024-11-27 07:09:15 +0530

    Initial project

---
+++ src/test/java/com/meditrack/meditrack_notifi_service/MeditrackNotifiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meditrack.meditrack_notifi_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeditrackNotifiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
